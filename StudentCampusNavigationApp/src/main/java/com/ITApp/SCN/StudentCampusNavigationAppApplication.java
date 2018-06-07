aefb276b140392b396158f62a7d66eabe671786f
dulek55 <maciekdul95@gmail.com>
2018-06-07 21:31:26 +0200

    1.0

---
+++ StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/StudentCampusNavigationAppApplication.java
@@ -0,0 +1,12 @@
+package com.ITApp.SCN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCampusNavigationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCampusNavigationAppApplication.class, args);
+	}
+}

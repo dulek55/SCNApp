0832a89b5775c6206c83967b5945a24311ea4999
dulek55 <maciekdul95@gmail.com>
2018-06-09 15:25:18 +0200

    2.1 - added BuildingsModelRepository and CrudRepo -> JpaRepo

---
+++ StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/repository/BuildingsModelRepository.java
@@ -0,0 +1,11 @@
+package com.ITApp.SCN.repository;
+
+import com.ITApp.SCN.models.BuildingsModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "buildings", path = "buildings")
+public interface BuildingsModelRepository extends JpaRepository<BuildingsModel, Long>{
+
+}
--- StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/repository/MessageModelRepository.java
+++ StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/repository/MessageModelRepository.java
@@ -1,10 +1,11 @@
 package com.ITApp.SCN.repository;
 
 import com.ITApp.SCN.models.MessageModel;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "messages", path = "messages")
-public interface MessageModelRepository extends CrudRepository<MessageModel, Long> {
+public interface MessageModelRepository extends JpaRepository<MessageModel, Long> {
 
 }

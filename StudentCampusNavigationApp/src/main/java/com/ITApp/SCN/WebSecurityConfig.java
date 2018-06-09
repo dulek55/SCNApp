5e20de687d19ca4bb52037df5064bac0fd749aaa
stromy <stromy@protonmail.com>
2018-06-09 17:50:16 +0200

    2.5.2

--- StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/WebSecurityConfig.java
+++ StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.ITApp.SCN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,7 +34,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/registration").permitAll()
-                    .anyRequest().authenticated()
                     .and()
                 .formLogin()
                     .loginPage("/login")

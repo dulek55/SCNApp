6329aa903393973ddf1bf8c058683246c3319652
dulek55 <maciekdul95@gmail.com>
2018-06-09 23:53:27 +0200

    3.2 - added ChatController

--- StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/controllers/ChatController.java
+++ StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/controllers/ChatController.java
@@ -2,7 +2,13 @@ package com.ITApp.SCN.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ITApp.SCN.models.ChatModel;
+import com.ITApp.SCN.models.UserModel;
 import com.ITApp.SCN.services.ChatModelService;
 import com.ITApp.SCN.services.MessageModelService;
 import com.ITApp.SCN.services.UserModelService;
@@ -18,4 +24,18 @@ public class ChatController {
 	
 	@Autowired
 	private ChatModelService chatModelService;
+	
+	@RequestMapping(value = "/joinChat", method = RequestMethod.POST, consumes = {"application/x-www-form-urlencoded"})
+	@ResponseBody
+    public String joinChat(@RequestParam("username") String username, @RequestParam("chatName") String chatname) {
+    	
+    	UserModel userModel = new UserModel();
+    	userModel = userModelService.findByName(username);
+    	ChatModel chatModel = new ChatModel();
+    	chatModel = chatModelService.findByName(chatname);
+    	
+    	userModelService.addUserToChat(userModel, chatModel);
+    	
+		return "Joined chat successfully";
+    }
 }

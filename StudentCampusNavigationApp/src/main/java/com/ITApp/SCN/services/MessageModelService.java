78d5865f4510f6ec446baa772001b4831153e3e9
dulek55 <maciekdul95@gmail.com>
2018-06-09 18:59:03 +0200

    2.5.5 - start work on MessageModel

--- StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/services/MessageModelService.java
+++ StudentCampusNavigationApp/src/main/java/com/ITApp/SCN/services/MessageModelService.java
@@ -1,45 +1,10 @@
 package com.ITApp.SCN.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.ITApp.SCN.models.MessageModel;
-import com.ITApp.SCN.repository.MessageModelRepository;
-import com.ITApp.SCN.repository.UserModelRepository;
+import com.ITApp.SCN.models.RoleModel;
+import com.ITApp.SCN.models.UserModel;
 
-@Service
-public class MessageModelService {
-	@Autowired
-    private MessageModelRepository messageRepository;
-    @Autowired
-    private UserModelRepository userRepository;
-
-    public void addMessage(MessageModel message) {
-
-        messageRepository.save(message);
-
-    }
-
-    /*public void updateCanoe(CanoeModel canoe, int id) {
-        CanoeModel currentCanoe = canoeRepository.findById(id).get();
-        currentCanoe.setPhoto(canoe.getPhoto());
-        currentCanoe.setCapacity(canoe.getCapacity());
-        currentCanoe.setColour(canoe.getColour());
-
-        canoeRepository.save(currentCanoe);
-    }*/
-
-    /*public void rentCanoe(int id, UserModel user) {
-            CanoeModel currentCanoe = canoeRepository.findById(id).get();
-        currentCanoe.setTakenBy(userRepository.findById(user.getId()).get());
-
-        canoeRepository.save(currentCanoe);
-    }*/
-
-    /*public void returnCanoe(int id, UserModel user) {
-        CanoeModel currentCanoe = canoeRepository.findById(id).get();
-        currentCanoe.setTakenBy(null);
-
-        canoeRepository.save(currentCanoe);
-    }*/
+public interface MessageModelService {
+	public void addMessageToUser(UserModel sender, UserModel receiver, MessageModel message);
+	public void addMessageToRole(UserModel sender, RoleModel receiverRole, MessageModel message);
 }
